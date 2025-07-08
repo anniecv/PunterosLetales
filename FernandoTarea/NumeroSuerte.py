dbc0e5f44a8d6727c31de0c5377aab63b4f2ea45
FernandoNavia-Nova <fernandonavianova26.fnn@gmail.com>
2025-07-08 23:26:11 +0000

    Tarea

---
+++ FernandoTarea/NumeroSuerte.py
@@ -0,0 +1,8 @@
+while True:
+    numero = int(input("Introduce un número: "))
+
+    if numero == 7:
+        print("¡Felicidades! Encontraste el número correcto.")
+        break  
+    else:
+        print(" Ese no es el número. Sigue intentando...\n")
