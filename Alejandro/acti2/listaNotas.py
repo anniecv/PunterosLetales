ffbdad30d3f5c50ec7aca2bd4bcbdc4c94fb8ecc
AlE <131571036+Alejandr026@users.noreply.github.com>
2025-07-10 16:29:17 -0400

    subida archivos actividad 2

---
+++ Alejandro/acti2/listaNotas.py
@@ -0,0 +1,8 @@
+mis_notas = [10, 10, 10, 10, 10]
+sum_total = 0
+for i in mis_notas:
+    sum_total = sum_total + i
+
+promedio = sum_total / len(mis_notas)
+print(f"La suma total de todas las notas es de: {sum_total}")
+print(f"El promedio de las notas es de: {promedio}")
