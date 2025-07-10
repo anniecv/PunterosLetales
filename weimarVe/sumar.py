5f66713a012b85debeb4c12498ceb653891a2df1
weimar295 <150382040+weimar295@users.noreply.github.com>
2025-07-10 00:02:28 +0000

    actividades

--- weimarVe/sumar.py
+++ weimarVe/sumar.py
@@ -5,9 +5,16 @@ def sumar(a, b):
   resultado_suma = a + b
   return resultado_suma
 
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+resultado = sumar(num1, num2)
+print(f"La suma de {num1} y {num2} es: {resultado}")
+
 assert sumar(2, 3) == 5, "Error: 2 + 3 debe ser 5"
 assert sumar(-1, 1) == 0, "Error: -1 + 1 debe ser 0"
 assert sumar(10, 0) == 10, "Error: 10 + 0 debe ser 10"
 
 print("Pruebas unitarias para sumar() superadas.")
-print   ("---Fin del programa---Weimar Valda--")
+print("---Fin del programa---Weimar Valda---")
+
