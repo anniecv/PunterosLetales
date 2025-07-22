3a07823fe27828c68f80fb5f9b12199f51c01a34
FernandoNavia-Nova <fernandonavianova26.fnn@gmail.com>
2025-07-22 21:28:53 +0000

    tarea

--- FernandoTarea/ListaNomEstudiante.py
+++
@@ -1,4 +0,0 @@
-lista_nomb = ["nomb1", "nomb2", "nomb3", "nomb4", "nomb5"]
-
-for i in lista_nomb:
-    print(f"Bienvenido al equipo {i} ")
--- FernandoTarea/ListadeNota.py
+++ FernandoTarea/ListadeNota.py
@@ -6,3 +6,4 @@ for i in mis_notas:
 promedio = sum_total / len(mis_notas)
 print(f"La suma total de todas las notas es de: {sum_total}")
 print(f"El promedio de las notas es de: {promedio}")
+print("-----------Fernando Navia Nova-----------")
